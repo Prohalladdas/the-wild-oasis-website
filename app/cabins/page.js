e0bcaff73c98cea4e6576c8958d60b967b290386
Prohalladdas <prolladdas@gmail.com>
2025-07-11 13:55:34 +0530

    Project Organization(Start)⭐

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,7 @@
-import Counter from "../components/counter";
-
-export default async function Page() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await res.json()
-    console.log(data)
+export default function Page() {
     return (
         <div>
             <h2>Cabin page</h2>
-            <ul>{data.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
-
-            <Counter users={data} />
         </div>
     );
 }
