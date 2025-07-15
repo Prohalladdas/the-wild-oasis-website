cb6e3e3c2aef92352afeafe160e742e45eef7272
Prohalladdas <prolladdas@gmail.com>
2025-07-15 21:13:19 +0530

    Improving the Navigation and Root Layout by styling

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from "@/app/_components/Navigation";
 
 import { Josefin_Sans } from "next/font/google"
 import "@/app/_styles/globals.css"
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -19,13 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return <html lang="en">
-    <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-      <header>
-        <Logo />
-        <Navigation />
-      </header>
-      <main>{children}</main>
-      <footer>@Copyright by the wild oasis</footer>
+    <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+
+      <Header />
+
+      <div className="flex-1 px-8 py-12">
+        <main className="max-w-7xl mx-auto">{children}</main>
+      </div>
     </body>
   </html>
 }
