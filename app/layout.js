5cc55329bc8db558258940bb561bf787b92d070e
Prohalladdas <prolladdas@gmail.com>
2025-07-16 23:47:00 +0530

    Building the Home Page

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
 import { Josefin_Sans } from "next/font/google"
-import "@/app/_styles/globals.css"
 import Header from "./_components/Header";
+import "@/app/_styles/globals.css"
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -20,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return <html lang="en">
-    <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+    <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
 
       <Header />
 
