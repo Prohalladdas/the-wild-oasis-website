b59dd6a72c7a05e299472023708b6bfe02e2d2fc
Prohalladdas <prolladdas@gmail.com>
2025-07-19 13:43:07 +0530

    Streaming Route Segments With loading.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import { Josefin_Sans } from "next/font/google"
+import { Josefin_Sans } from "next/font/google";
 import Header from "./_components/Header";
-import "@/app/_styles/globals.css"
+import "@/app/_styles/globals.css";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -12,18 +12,22 @@ export const metadata = {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
-  description: "Luxurious cabin hotel, located in the heart of the Indian Ocean, sorrounded by beautiful nature and wildlife."
+  description:
+    "Luxurious cabin hotel, located in the heart of the Indian Ocean, sorrounded by beautiful nature and wildlife.",
 };
 
 export default function RootLayout({ children }) {
-  return <html lang="en">
-    <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
 
-      <Header />
-
-      <div className="flex-1 px-8 py-12 grid">
-        <main className="max-w-7xl mx-auto w-full">{children}</main>
-      </div>
-    </body>
-  </html>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
 }
