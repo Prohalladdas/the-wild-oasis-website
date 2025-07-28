67f32a34fb0437ead58cb5c0ecb33af2412011ab
Prohalladdas <prolladdas@gmail.com>
2025-07-28 20:00:44 +0530

    Building a Custom Sign In Page

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,16 @@
-export const metadata = {
-    title: "Guest area"
-}
+import { auth } from "../_lib/auth";
 
-export default function Page() {
-    return <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Prohallad
+export const metadata = {
+  title: "Guest area",
+};
+
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
     </h2>
+  );
 }
