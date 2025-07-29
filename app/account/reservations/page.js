8d3076290cb11391b3b1a9c3b9a5070d23448870
Prohalladdas <prolladdas@gmail.com>
2025-07-29 13:52:12 +0530

    Building the Guest's Reservations Page

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,16 +1,17 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
 
 export const metadata = {
-  title: "Reservations"
+  title: "Reservations",
 };
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
-
       {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our
